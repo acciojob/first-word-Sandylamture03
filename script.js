1647cf81d875830063a0882898c8d716e3b4847a
Sandy_Lamture <sandylamture03@gmail.com>
2025-06-25 22:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading spaces
+  str = str.trim();
+  
+  // Check if the string is empty
+  if (str === '') {
+    return '';
+  }
+  
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+  
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+  
+  // Return the substring from the start to the first space
+  return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(str));
